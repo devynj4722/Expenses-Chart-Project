bd3d5ab2ea11a15229a837fa8e6274f639ff509e
devynj4722 <jmoneyaae@gmail.com>
2022-09-14 14:55:37 -0700

    added use state

--- src/App.jsx
+++ src/App.jsx
@@ -11,46 +11,37 @@ function App(){
   for(let i=0; i < 7; i++){
     let days = "day" + i + ""
     
-     console.log(days)
+  
   }
   console.log()
- const [isOn, SetIsOn] = useState(false);
+ const [isHover, SetIsHover] = useState(false);
 
- /** Mouse Enter Function */
-  const toggleBarClass = (e) => {
-    
-  console.log(myNodes)
-   
-  e.preventDefault();
-  console.log(e.target)
- for(let i=0; i < myNodes.length; i++){
-
-  if (e.target == myNodes[i]) {
-
-    document.getElementById('day' + i ).classList.remove("test")
-    document.getElementById('day' + i).classList.add("popup-active")
-   
-  } 
- }
-  
-  
-
-  };
-// Mouse Leave Function 
-  const deactiveBarClass = (e) => {
-    e.preventDefault();
-     for(let i=0; i < myNodes.length; i++){
-
-      if (e.target = myNodes[i]) {
-        document.getElementById('day' + i ).classList.remove("popup-active")
-        document.getElementById('day' + i ).classList.add("test")
-   
-  } 
-    
+const setHoverClass = (e) => {
+  console.log(e.target.classList)
+  SetIsHover(true)
  
-   
+  for(let i=0; i < myNodes.length; i++){
+
+    if (e.target == myNodes[i]) {
+  
+      document.getElementById('day' + i ).classList.remove("test")
+      document.getElementById('day' + i).classList.add("popup-active")
+     
+    }
+     
   }
   
+}
+const disableHoverClass = (e) => {
+  SetIsHover(false)
+  for(let i=0; i < myNodes.length; i++){
+
+    if (e.target = myNodes[i]) {
+      document.getElementById('day' + i ).classList.remove("popup-active")
+      document.getElementById('day' + i ).classList.add("test")
+ 
+} 
+  }
 }
 return (
   <div className='root-container'>
@@ -62,14 +53,14 @@ return (
 </section>
 <section className="graphs">
   <h1 id="spending">Spending - Last 7 Days</h1>
-  <section className='days' onMouseEnter={toggleBarClass} onMouseLeave={deactiveBarClass}>
-<div id="1"><div className='test' id="day0" >$17.45</div><div onMouseOver={toggleBarClass} onMouseLeave={deactiveBarClass} className="bar" id="bar1"></div>mon</div>
-<div id="2"><div className='test'id="day1">$34.91</div><div onMouseOver={toggleBarClass} onMouseLeave={deactiveBarClass} className="bar" id="bar2"></div>tues</div>
-<div id="3"><div className="test" id='day2'>$52.36</div><div onMouseOver={toggleBarClass} onMouseLeave={deactiveBarClass} className="bar" id="bar3"></div>wens</div>
-<div id="4"><div className="test" id='day3'>$31.07</div><div onMouseOver={toggleBarClass} onMouseLeave={deactiveBarClass} className="bar" id="bar4"></div>thu</div>
-<div id="5"><div className="test" id='day4'>$23.39</div><div onMouseOver={toggleBarClass} onMouseLeave={deactiveBarClass} className="bar" id="bar5"></div>fri</div>
-<div id="6"><div className="test" id='day5'>$43.28</div><div  onMouseOver={toggleBarClass} onMouseLeave={deactiveBarClass} className="bar" id="bar6"></div>sat</div>
-<div id="7"><div className="test" id='day6'>$25.48</div><div onMouseOver={toggleBarClass} onMouseLeave={deactiveBarClass}  className="bar" id="bar7"></div>sun</div>
+  <section className='days' onMouseEnter={setHoverClass} onMouseLeave={disableHoverClass}>
+<div id="1"><div className='test' id="day0" >$17.45</div><div onMouseOver={setHoverClass} onMouseLeave={disableHoverClass} className="bar" id="bar1"></div>mon</div>
+<div id="2"><div className='test'id="day1">$34.91</div><div onMouseOver={setHoverClass} onMouseLeave={disableHoverClass} className="bar" id="bar2"></div>tues</div>
+<div id="3"><div className="test" id='day2'>$52.36</div><div onMouseOver={setHoverClass} onMouseLeave={disableHoverClass} className="bar" id="bar3"></div>wens</div>
+<div id="4"><div className="test" id='day3'>$31.07</div><div onMouseOver={setHoverClass} onMouseLeave={disableHoverClass} className="bar" id="bar4"></div>thu</div>
+<div id="5"><div className="test" id='day4'>$23.39</div><div onMouseOver={setHoverClass} onMouseLeave={disableHoverClass} className="bar" id="bar5"></div>fri</div>
+<div id="6"><div className="test" id='day5'>$43.28</div><div  onMouseOver={setHoverClass} onMouseLeave={disableHoverClass} className="bar" id="bar6"></div>sat</div>
+<div id="7"><div className="test" id='day6'>$25.48</div><div onMouseOver={setHoverClass} onMouseLeave={disableHoverClass}  className="bar" id="bar7"></div>sun</div>
   </section>
 </section>
 <section className="footer">
@@ -89,6 +80,7 @@ return (
   </main>
   </div>
 )
+
 }
 
 export default App
